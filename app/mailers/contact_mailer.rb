eee8f6329f7d3d1d4696bb7b615f4b7cbbfdc759
tomohiro.kojima <niniseseko@gmail.com>
2021-10-20 20:24:53 +0000

    お問い合わせ用のメイラーを設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,24 +1,20 @@
 class ContactMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.user_email.subject
-  #
-  def user_email(name:, email:)
-    @name = name
-    mail(
-      to: email,
-      subject: "【アプリ名】お問い合わせを受付いたしました"
-    )
+  ADMIN_EMAIL = "teachingmatrialsapp@gmail.com"
+
+  def user_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【アプリ名】お問い合わせを受付いたしました"
+
+    mail(to: contact.email, subject: subject)
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.admin_email.subject
-  #
-  def admin_email
-    
+  def admin_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【アプリ名】お問い合わせがありました"
+
+    mail(to: ADMIN_EMAIL, subject: subject)
   end
 end
